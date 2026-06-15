0ba672e5928f1f43caab30870cc9f9c32239990b
sazydev <qsweez17@gmail.com>
2026-06-15 02:28:08 +0200

    use vite supabase env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://uofjcoialctfosiqjqdq.supabase.co";
-
-const supabaseKey = "sb_publishable_sMf0kXANVtll90zAScsPtQ_2Pqvtw6R";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
